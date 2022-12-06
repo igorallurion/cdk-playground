faa81000764a46f13814b63879743aa1f1829853
Igor Costa <icosta@allurion.com>
2022-12-06 11:31:22 -0300

    feat(api-gateway): setup authorizer

--- examples/api-gateway/lib/api-gateway-stack.ts
+++ examples/api-gateway/lib/api-gateway-stack.ts
@@ -1,12 +1,19 @@
 import * as cdk from "aws-cdk-lib";
+import { Duration } from "aws-cdk-lib";
 import {
   Cors,
+  IAuthorizer,
+  IdentitySource,
   IResource,
   MockIntegration,
   PassthroughBehavior,
+  RequestAuthorizer,
+  ResponseType,
   RestApi,
 } from "aws-cdk-lib/aws-apigateway";
+import * as lambda from "aws-cdk-lib/aws-lambda";
 import { Construct } from "constructs";
+import { join } from "path";
 
 type ApiGatewayStackProps = cdk.StackProps & {
   allowedOrigins: string[];
@@ -31,13 +38,49 @@ export class ApiGatewayStack extends cdk.Stack {
       },
     });
 
-    this.createMockedMethod(api.root, "GET", allowedOrigins);
+    // api.addGatewayResponse("MissingAuthenticationResponse", {
+    //   type: ResponseType.MISSING_AUTHENTICATION_TOKEN,
+    //   statusCode: "401",
+    //   responseHeaders: {
+    //     "Access-Control-Allow-Origin": "*",
+    //   },
+    // });
 
-    this.createResourceNotFoundHandler(api);
+    api.addGatewayResponse("UnauthorizedResponse", {
+      type: ResponseType.UNAUTHORIZED,
+      statusCode: "403",
+      responseHeaders: {
+        "Access-Control-Allow-Origin": "*",
+      },
+      // templates: {
+      //   "application/json":
+      //     '{ "message": $context.authMessage, "statusCode": $context.authStatusCode }',
+      // },
+    });
+
+    const authorizerLambda = new lambda.Function(this, "Authorizer", {
+      runtime: lambda.Runtime.NODEJS_16_X,
+      handler: "index.handler",
+      code: this.createInlineAuthorizerCode(),
+    });
+
+    const authorizer = new RequestAuthorizer(this, "custom-authorizer", {
+      handler: authorizerLambda,
+      identitySources: [IdentitySource.header("Authorization")],
+      resultsCacheTtl: Duration.seconds(0),
+    });
+
+    this.createMockedMethod(api.root, "GET", allowedOrigins, authorizer);
+
+    this.createResourceNotFoundHandler(api.root);
   }
 
-  private createResourceNotFoundHandler(api: cdk.aws_apigateway.RestApi) {
-    const proxyResouce = api.root.addResource("{proxy+}");
+  private createInlineAuthorizerCode(): cdk.aws_lambda.Code {
+    return lambda.Code.fromAsset(join(__dirname, "authorizer"));
+  }
+
+  private createResourceNotFoundHandler(baseResource: IResource) {
+    const proxyResouce = baseResource.addResource("{proxy+}");
     proxyResouce.addMethod(
       "GET",
       new MockIntegration({
@@ -63,7 +106,8 @@ export class ApiGatewayStack extends cdk.Stack {
   private createMockedMethod(
     resource: IResource,
     method: string,
-    allowedOrigins: string[]
+    allowedOrigins: string[],
+    authorizer?: IAuthorizer
   ) {
     const integrationResponseParams =
       this.createIntegrationResponse(allowedOrigins);
@@ -103,16 +147,15 @@ export class ApiGatewayStack extends cdk.Stack {
             responseParameters: methodResponseParams,
           },
         ],
+        authorizer,
       }
     );
   }
 
-  createIntegrationResponse(allowedOrigins: string[]) {
+  private createCorsHeaders(allowedOrigins: string[]): Record<string, string> {
     const initialOrigin = allowedOrigins[0];
 
-    const integrationResponseParams: { [p: string]: string } = {};
-    const methodResponseParams: { [p: string]: boolean } = {};
-    const headers: { [name: string]: string } = {
+    const headers: Record<string, string> = {
       "Access-Control-Allow-Headers":
         "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token,X-Amz-User-Agent'",
       "Access-Control-Allow-Methods":
@@ -128,10 +171,16 @@ export class ApiGatewayStack extends cdk.Stack {
       headers["Vary"] = "'Origin'";
     }
 
+    return headers;
+  }
+
+  createIntegrationResponse(allowedOrigins: string[]) {
+    const integrationResponseParams: Record<string, string> = {};
+    const headers = this.createCorsHeaders(allowedOrigins);
+
     for (const [name, value] of Object.entries(headers)) {
       const key = `method.response.header.${name}`;
       integrationResponseParams[key] = value;
-      methodResponseParams[key] = true;
     }
 
     return integrationResponseParams;
